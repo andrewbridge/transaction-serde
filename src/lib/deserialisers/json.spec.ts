792d56dd3e837cda74b2fdfa3fd359a59e108733
Andrew Bridge <andrew@andrewhbridge.co.uk>
2025-02-14 18:40:27 +0000

    feat: add further testing to get full line coverage for JSON deserialiser

--- src/lib/deserialisers/json.spec.ts
+++ src/lib/deserialisers/json.spec.ts
@@ -1,5 +1,6 @@
 import { UTCDateMini } from '@date-fns/utc';
 import test from 'ava';
+import { Transaction } from 'transaction-serde';
 
 import json from './json';
 
@@ -21,6 +22,30 @@ const DATA = `[
     {
         "date": "2024-04-02",
         "amount": -100,
+        "payee": "Energy",
+        "description": "A-0A00AA00-001",
+        "category": "Bills"
+    }
+]`;
+
+const PARTIALLY_INVALID_DATA = `[
+    {
+        "date": "2024-04-01",
+        "amount": 134.99,
+        "payee": "Acme",
+        "description": "Acme Salary April",
+        "category": "Income"
+    },
+    {
+        "date": 20240402,
+        "amount": -34.99,
+        "payee": "Internet",
+        "description": "INET12345678-0",
+        "category": "Bills"
+    },
+    {
+        "date": "2024-04-02",
+        "amount": "$100",
         "payee": "Energy",
         "description": "A-0A00AA00-001",
         "category": "Bills"
@@ -51,4 +76,15 @@ test('deserialising json', (t) => {
       category: 'Bills',
     },
   ]);
+});
+
+test('json deserialiser throws on invalid data', (t) => {
+  // Unparseable data
+  t.throws(() => json('invalid'));
+  // Not an array
+  t.throws(() => json('{}'));
+});
+
+test('json deserialiser ignores items with invalid data types', (t) => {
+  t.true((json(PARTIALLY_INVALID_DATA) as Transaction[]).length === 1);
 });
